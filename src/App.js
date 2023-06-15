7c9340a3e43ce60262faad6eee9918d75fc82604
wsYYnb3 <danielisraeli033@gmail.com>
2023-06-15 17:24:45 +0200

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-dark">
+      <div className="container">
+        <p className="text-center text-white">© 2023 My Grocery Store. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
